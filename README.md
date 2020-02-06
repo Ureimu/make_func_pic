2a3b0e6c019df8d7f8ae440bc2dc1f7b537c5217
Ureimu <a1090693441@163.com>
2020-02-06 12:27:29 +0800

    .

--- README.md
+++ README.md
@@ -18,5 +18,9 @@ With these two parameter and some parameter equations, you can get a pretty gif.
     padding: 2px;"> </div>
 </center>
 
+<hr2>使用说明:<hr2>
 
+下载下来的文件解压后在make_func_pic文件夹里按住shift再按右键,可以看到有一个'在此处打开命令行窗口',点击以后输入'python main.py'就可以运行了.
+
+接下来只需要按照自己的要求输入相关信息就可以制作gif了.
 
--- preconfig.py
+++ preconfig.py
@@ -1,17 +1,20 @@
 
 def read_preconfig():  # 读取用户的preconfig文件
     file = open('preconfig.txt', 'r')
-    copy0 = file.readline() - '\n'
+    copylist = []
+    copy0 = file.readline()
+    copylist = copy0.split('\n')
+    print(copy0)
     facecolor = file.readline()
     if facecolor == '':
         return 0
     print(copy0,facecolor)
     file.close()
-    preconfigdict = {'copy0':copy0,'facecolor':facecolor}
+    preconfigdict = {'copy0':copylist[0],'facecolor':facecolor}
     return preconfigdict
 
 
-def write_preconfig_default():  #　创建一个默认的preconfig文件
+def write_preconfig_default():   # 创建一个默认的preconfig文件
     file = open('preconfig.txt','x')
     file.write('pic/\n')
     copy0 = 'pic/'
